cd3b7ea5a898ce69fafbc6e95de8142e4f9978ab
Sárándi <sarandi.balint@gmail.com>
2022-10-20 13:15:36 +0200

    input mezők feltöltése + ha nincs kitöltve valamelyik akkor ugrál

--- src/index.js
+++ src/index.js
@@ -14,19 +14,72 @@ let sportok = [
     new calvinball_1.Calvinball('Hobbes', new Date(2134, 3, 4)),
     new calvinball_1.Calvinball('Kelvin', new Date(1999, 9, 9)),
 ];
+let sportok2 = [];
+/*
 console.log("\nKiírás:\n");
-/* for(let sport of sportok){
+for(let sport of sportok){
     sport.result();
-} */
-for (let sport of sportok) {
-    if (sport.winner == "Calvin") {
+}
+
+for(let sport of sportok) {
+    if(sport.winner == "Calvin"){
         sport.result();
     }
 }
 console.log("\nRendezett tömb:\n");
 sportok.sort((a, b) => a.date.getTime() - b.date.getTime());
-for (let sport of sportok) {
+for(let sport of sportok){
     sport.result();
-}
+} */
 document.addEventListener("DOMContentLoaded", () => {
+    var _a, _b, _c, _d, _e, _f;
+    //jobbra-balra ugrál ha nincs kitöltve
+    (_a = document.getElementById('focisubmit')) === null || _a === void 0 ? void 0 : _a.addEventListener("mouseover", () => {
+        var _a;
+        if (document.getElementById('footballwinner').value == "" ||
+            document.getElementById('footballdate').value == "" ||
+            parseInt(document.getElementById('elso').value) == NaN ||
+            parseInt(document.getElementById('masodik').value) == NaN) {
+            if ((_a = document.getElementById('focisubmit')) === null || _a === void 0 ? void 0 : _a.classList.contains('jobb')) {
+                document.getElementById("focisubmit").classList.remove("jobb");
+            }
+            else
+                document.getElementById("focisubmit").classList.add("jobb");
+        }
+    });
+    (_b = document.getElementById('marathonsubmit')) === null || _b === void 0 ? void 0 : _b.addEventListener("mouseover", () => {
+        var _a;
+        if (document.getElementById('marathonwinner').value == "" ||
+            document.getElementById('marathondate').value == "" ||
+            parseInt(document.getElementById('ido').value) == NaN) {
+            if ((_a = document.getElementById('marathonsubmit')) === null || _a === void 0 ? void 0 : _a.classList.contains('jobb')) {
+                document.getElementById("marathonsubmit").classList.remove("jobb");
+            }
+            else
+                document.getElementById("marathonsubmit").classList.add("jobb");
+        }
+    });
+    (_c = document.getElementById('calvinsubmit')) === null || _c === void 0 ? void 0 : _c.addEventListener("mouseover", () => {
+        var _a;
+        if (document.getElementById('calinballwinner').value == "" ||
+            document.getElementById('calvinballdate').value == "") {
+            if ((_a = document.getElementById('calvinsubmit')) === null || _a === void 0 ? void 0 : _a.classList.contains('jobb')) {
+                document.getElementById("calvinsubmit").classList.remove("jobb");
+            }
+            else
+                document.getElementById("calvinsubmit").classList.add("jobb");
+        }
+    });
+    (_d = document.getElementById('focisubmit')) === null || _d === void 0 ? void 0 : _d.addEventListener('click', () => {
+        sportok2.push(new football_1.Football(document.getElementById('footballwinner').value, new Date(document.getElementById('footballdate').value), parseInt(document.getElementById('elso').value), parseInt(document.getElementById('masodik').value)));
+        console.log(sportok2[sportok2.length - 1].toString());
+    });
+    (_e = document.getElementById('marathonsubmit')) === null || _e === void 0 ? void 0 : _e.addEventListener('click', () => {
+        sportok2.push(new marathon_1.Marathon(document.getElementById('marathonwinner').value, new Date(document.getElementById('marathondate').value), parseInt(document.getElementById('ido').value)));
+        console.log(sportok2[sportok2.length - 1].toString());
+    });
+    (_f = document.getElementById('calvinsubmit')) === null || _f === void 0 ? void 0 : _f.addEventListener('click', () => {
+        sportok2.push(new calvinball_1.Calvinball(document.getElementById('marathonwinner').value, new Date(document.getElementById('marathondate').value)));
+        console.log(sportok2[sportok2.length - 1].toString());
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -15,26 +15,77 @@ let sportok: Result[] = [
     new Calvinball('Kelvin', new Date(1999, 9, 9)),
 ];
 
+let sportok2: Result[] = [];
+/*
 console.log("\nKiírás:\n");
-
-/* for(let sport of sportok){
+for(let sport of sportok){
     sport.result();
-} */
+}
 
 for(let sport of sportok) {
     if(sport.winner == "Calvin"){
         sport.result();
     }
 }
-
 console.log("\nRendezett tömb:\n");
-
 sportok.sort((a, b) => a.date.getTime() - b.date.getTime());
-
 for(let sport of sportok){
     sport.result();
-}
+} */
 
 document.addEventListener("DOMContentLoaded", () =>{
-    
+    //jobbra-balra ugrál ha nincs kitöltve
+    document.getElementById('focisubmit')?.addEventListener("mouseover", () =>{
+        if((document.getElementById('footballwinner') as HTMLInputElement).value == "" || 
+        (document.getElementById('footballdate') as HTMLInputElement).value == "" ||
+        parseInt((document.getElementById('elso') as HTMLInputElement).value) == NaN || 
+        parseInt((document.getElementById('masodik') as HTMLInputElement).value) == NaN){
+            if(document.getElementById('focisubmit')?.classList.contains('jobb')){
+                (document.getElementById("focisubmit") as HTMLInputElement).classList.remove("jobb");
+            }else (document.getElementById("focisubmit") as HTMLInputElement).classList.add("jobb");
+        }
+    });
+    document.getElementById('marathonsubmit')?.addEventListener("mouseover", () =>{
+        if((document.getElementById('marathonwinner') as HTMLInputElement).value == "" || 
+        (document.getElementById('marathondate') as HTMLInputElement).value == "" ||
+        parseInt((document.getElementById('ido') as HTMLInputElement).value) == NaN){
+            if(document.getElementById('marathonsubmit')?.classList.contains('jobb')){
+                (document.getElementById("marathonsubmit") as HTMLInputElement).classList.remove("jobb");
+            }else (document.getElementById("marathonsubmit") as HTMLInputElement).classList.add("jobb");
+        }
+    });
+    document.getElementById('calvinsubmit')?.addEventListener("mouseover", () =>{
+        if((document.getElementById('calinballwinner') as HTMLInputElement).value == "" || 
+        (document.getElementById('calvinballdate') as HTMLInputElement).value == ""){
+            if(document.getElementById('calvinsubmit')?.classList.contains('jobb')){
+                (document.getElementById("calvinsubmit") as HTMLInputElement).classList.remove("jobb");
+            }else (document.getElementById("calvinsubmit") as HTMLInputElement).classList.add("jobb");
+        }
+    });
+
+
+
+
+
+    document.getElementById('focisubmit')?.addEventListener('click', () =>{
+        sportok2.push(new Football(
+            (document.getElementById('footballwinner') as HTMLInputElement).value, 
+            new Date((document.getElementById('footballdate') as HTMLInputElement).value), 
+            parseInt((document.getElementById('elso') as HTMLInputElement).value), 
+            parseInt((document.getElementById('masodik') as HTMLInputElement).value)));
+        console.log(sportok2[sportok2.length-1].toString());
+    });
+    document.getElementById('marathonsubmit')?.addEventListener('click', () =>{
+        sportok2.push(new Marathon(
+            (document.getElementById('marathonwinner') as HTMLInputElement).value, 
+            new Date((document.getElementById('marathondate') as HTMLInputElement).value), 
+            parseInt((document.getElementById('ido') as HTMLInputElement).value)))
+        console.log(sportok2[sportok2.length-1].toString());
+    });
+    document.getElementById('calvinsubmit')?.addEventListener('click', () =>{
+        sportok2.push(new Calvinball(
+            (document.getElementById('marathonwinner') as HTMLInputElement).value, 
+            new Date((document.getElementById('marathondate') as HTMLInputElement).value)))
+        console.log(sportok2[sportok2.length-1].toString());
+    });
 });
